b8bf91b9530977308ab8e89f84a36b4e3bace38c
Songhyeon Seo <songhyeon4643@gmail.com>
2022-07-11 20:13:06 +0900

    Create 10869.java

---
+++ baekjoon_algorithm/10869.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Operation {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("µÎ Á¤¼ö¸¦ ÀÔ·ÂÇÏ¼¼¿ä >> ");
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		
+		System.out.println(A+B);
+		System.out.println(A-B);
+		System.out.println(A*B);
+		System.out.println(A/B);
+		System.out.println(A%B);
+		
+		
+		
+
+	}
+
+}

dc6f0349d48d4ccfe12c83544e3d3ca5eb89719a
Songhyeon Seo <songhyeon4643@gmail.com>
2022-07-11 20:11:58 +0900

    Create 9498.java

---
+++ baekjoon_algorithm/9498.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class TestGrade {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Á¡¼ö¸¦ ÀÔ·ÂÇÏ¼¼¿ä >> ");
+		
+		int score = scanner.nextInt();
+		
+		if (score >= 90 && score <= 100)
+	    {
+	        System.out.println( "A");
+	    }
+	    else if (score >= 80 && score <= 89)
+	    {
+	    	System.out.println( "B");
+	    }
+	    else if (score >= 70 && score <= 79)
+	    {
+	    	System.out.println( "C");
+	    }
+	    else if (score >= 60 && score <= 69)
+	    {
+	    	System.out.println( "D");
+	    }
+	    else
+	    {
+	    	System.out.println( "F");
+	    }
+
+		
+
+	}
+
+}

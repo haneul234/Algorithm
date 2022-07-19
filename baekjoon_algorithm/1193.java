2d79842a7f2ff74270ea2bd71afa89ed0f3e6df5
Songhyeon Seo <songhyeon4643@gmail.com>
2022-07-19 15:15:26 +0900

    Create 1193.java

---
+++ baekjoon_algorithm/1193.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int i=1;
+        while(num>i){
+            num-=i;
+            i++;
+        }
+
+        if(i%2==1){
+            System.out.println((i+1-num)+"/"+num);
+        }else{
+            System.out.println(num+"/"+(i+1-num));
+        }
+    }
+}

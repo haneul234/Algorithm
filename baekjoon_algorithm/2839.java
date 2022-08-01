4bbe386edad6d8d12cc3d11211936b3a672f1fa2
Songhyeon Seo <songhyeon4643@gmail.com>
2022-08-02 00:04:09 +0900

    Update 2839.java

--- baekjoon_algorithm/2839.java
+++ baekjoon_algorithm/2839.java
@@ -1,5 +1,27 @@
 import java.util.Scanner;
 
+public class Main{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+int N = sc.nextInt();
+int count =0;
+
+while(true){
+if(N%5==0){
+    System.out.println(N/5 +  count);
+    break;
+}else if(N<0){
+    System.out.println(-1);
+    break;
+}
+    N = N-3;
+    count++;
+}
+}
+}
+
+/*import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -17,4 +39,10 @@ public class Main {
         } else if (N % 5 == 2 || N % 5 == 4) {
             System.out.println((N / 5) + 2);
         }
-    }}
+    }}*/
+
+/*
+최소 설탕봉투를 옮기려면 큰 수인 5로 나누어야지 최소 봉투만 옮길 수 있다.
+5로 나누어 떨어지면 if문에 들어가고 아니라면 if문이 아닌 N=N-3; count++;문으로 이동해서 실행한다.
+n이 3과 5로 안 나누어지면 -1을 출력한다.
+*/

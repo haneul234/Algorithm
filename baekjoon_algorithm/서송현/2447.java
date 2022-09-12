8124afaaa668a560f9ebcfd7d853dce77b0d8f11
Songhyeon Seo <songhyeon4643@gmail.com>
2022-09-12 23:54:15 +0900

    Update 2447.java

--- baekjoon_algorithm/서송현/2447.java
+++ baekjoon_algorithm/서송현/2447.java
@@ -1,3 +1,5 @@
+
+~java
 import java.util.*;
 public class Baekjoon_2447 {
     public static void star(int i,int j,int n){
@@ -21,3 +23,32 @@ public class Baekjoon_2447 {
 
     }
 }
+~java
+
+~java
+import java.util.*;
+import java.io.*;
+public class Main {
+    static StringBuffer sb = new StringBuffer();
+    public static void star(int i,int j,int n){
+        if((i/n)%3==1&&(j/n)%3==1){
+            sb.append(" ");
+        }else {
+            if(n/3==0) sb.append("*");
+            else star(i,j,n/3);
+        }
+    }
+    public static void main (String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<n;i++){
+            for(int j=0;j<n;j++){
+                star(i,j,n);
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
+~java
